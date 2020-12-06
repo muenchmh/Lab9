c8b35b8c1942d74c126d5fddb2b3ae3ac92b0403
Mike Muenchen <michaelhmuenchen@gmail.com>
2020-12-06 04:06:23 +0000

    constructors

---
+++ Person.java
@@ -0,0 +1,43 @@
+class Person
+{
+  private String name;
+  private int age;
+  private String color;
+
+  Person (String aName, int anAge, String aColor)
+  {
+    name = aName;
+    age = anAge;
+    color = aColor;
+  }
+
+  String getName()
+  {
+    return name;
+  }
+
+  int getAge
+  {
+    return age;
+  }
+
+  String getColor
+  {
+    return color;
+  }
+
+  void setName(String aName)
+  {
+    name = aName;
+  }
+
+  void setAge(int anAge)
+  {
+    age = anAge;
+  }
+
+  void setColor(String aColor)
+  {
+    color = aColor;
+  }
+}
